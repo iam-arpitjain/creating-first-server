0d7eca8df5d9324c81ddccd4d18680a30aa0d15f
iam-arpitjain <jainarpit762@gmail.com>
2022-10-20 18:27:20 +0530

    routes are added

--- server.js
+++ server.js
@@ -2,10 +2,37 @@ const http = require('http'); //package import
 
 const port = 8081; //local port
 
+const toDoList = ["Playing Cricket", "Playing Valleyball"];
+
 http.createServer((request, response) => {
-    //callback function
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write("<h1> Hello, this is from my server </h1>");
+    const { method, url } = request;
+
+    if (url === "/todos") {
+        if (method === "GET") {
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.write(toDoList.toString());
+        }
+        else if (method === "POST") {
+            let body = "";
+            request.on('error', (err) => {
+                console.error(err);
+            })
+                .on('data', (chunk) => {
+                    body += chunk;
+                })
+                .on('end', () => {
+                    body = JSON.parse(body);
+                    console.log("data : ", body);
+                })
+        }
+        else {
+            response.writeHead(501);
+        }
+    }
+    else {
+        response.writeHead(404);
+    }
+
     response.end();
 }).listen(port, () => {
     //callback function
